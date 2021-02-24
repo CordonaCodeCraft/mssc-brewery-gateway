f00963bf3f078e59eee24489d2a02b2019a3f253
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-24 17:26:56 +0200

    failover config

--- src/main/java/com/sfg/gateway/config/GoogleConfig.java
+++ src/main/java/com/sfg/gateway/config/GoogleConfig.java
@@ -3,9 +3,10 @@ package com.sfg.gateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
+/**
+ * Created by jt on 3/5/20.
+ */
 //@Profile("google")
 //@Configuration
 public class GoogleConfig {
@@ -13,8 +14,8 @@ public class GoogleConfig {
     @Bean
     public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/googlesearch")
-                        .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
+                .route(r -> r.path("/googlesearch2")
+                        .filters(f -> f.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
                         .uri("https://google.com")
                         .id("google"))
                 .build();
